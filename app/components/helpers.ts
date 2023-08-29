9e8b813a41f9d5a3d3816880f8304ad92b62c0ea
rduraj96 <durajrei@gmail.com>
2023-08-28 21:26:21 -0400

    Updated helpers

--- app/components/helpers.ts
+++ app/components/helpers.ts
@@ -22,25 +22,32 @@ export function closeProgram(
 
 export function minimizeProgram(
   program: Program,
-  runningTasks: Program[],
-  setRunningTasks: Dispatch<SetStateAction<Program[]>>
+  setProgram: Dispatch<SetStateAction<Program>>
 ) {
-  console.log("Minimized Pressed");
-  setRunningTasks(
-    runningTasks.map((task, i) => {
-      if (task.id === program.id) {
-        return { ...task, isMinimized: !program.isMinimized };
-      }
-      return task;
-    })
-  );
+  if (program.isMinimized === true) {
+    setProgram((prevState) => ({
+      ...prevState,
+      zIndex: getMaxZIndex() + 1,
+    }));
+  }
+  setProgram((prevState) => ({
+    ...prevState,
+    isMinimized: !prevState.isMinimized,
+  }));
 }
 
-export function checkVisibility(
-  program: Program,
-  runningTasks: Program[]
-): boolean {
-  return (
-    runningTasks.find((task) => task.id === program.id)!.isMinimized || true
+export function focusProgram(setProgram: Dispatch<SetStateAction<Program>>) {
+  setProgram((prevState) => ({
+    ...prevState,
+    zIndex: getMaxZIndex() + 1,
+  }));
+}
+
+export function getMaxZIndex(): number {
+  return Math.max(
+    ...Array.from(document.querySelectorAll("body *"), (el) =>
+      parseFloat(window.getComputedStyle(el).zIndex)
+    ).filter((zIndex) => !Number.isNaN(zIndex)),
+    0
   );
 }
