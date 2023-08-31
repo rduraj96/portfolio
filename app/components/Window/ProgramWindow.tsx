3012871479c768a615b57837e6f7c55efcc86fd5
rduraj96 <durajrei@gmail.com>
2023-08-31 00:00:27 -0400

    Added focus flag

--- app/components/Window/ProgramWindow.tsx
+++ app/components/Window/ProgramWindow.tsx
@@ -23,14 +23,16 @@ const ProgramWindow = ({ children, program, setProgram, className }: Props) => {
 
   return (
     program.isOpen === true && (
-      <Draggable
-        onMouseDown={() => focusProgram(program, setProgram, setFocused)}
-      >
+      <Draggable handle="strong">
         <div
-          className={`window ${className}
+          className={`select-none window ${className}
           
           `}
           tabIndex={-1}
+          onClick={() => {
+            program.id !== focused &&
+              focusProgram(program, setProgram, setFocused);
+          }}
           onBlur={() => setFocused("")}
           style={{
             visibility: program.isMinimized === true ? "hidden" : "visible",
@@ -43,23 +45,28 @@ const ProgramWindow = ({ children, program, setProgram, className }: Props) => {
                 : "",
           }}
         >
-          <div
+          <strong
             className="title-bar py-3"
-            style={{
-              background:
-                program.id !== focused
-                  ? "linear-gradient(to bottom, #7697e7 0%,#7e9ee3 3%,#94afe8 6%,#97b4e9 8%,#82a5e4 14%,#7c9fe2 17%,#7996de 25%,#7b99e1 56%,#82a9e9 81%,#80a5e7 89%,#7b96e1 94%,#7a93df 97%,#abbae3 100%)"
-                  : "",
-              paddingTop: "12px",
-              paddingBottom: "12px",
-              borderTop: "1px solid #7b99e1",
-              borderLeft: "1px solid #7b99e1",
-              borderRight: "1px solid #7b99e1",
-            }}
+            style={
+              program.id !== focused
+                ? {
+                    background:
+                      "linear-gradient(to bottom, #7697e7 0%,#7e9ee3 3%,#94afe8 6%,#97b4e9 8%,#82a5e4 14%,#7c9fe2 17%,#7996de 25%,#7b99e1 56%,#82a9e9 81%,#80a5e7 89%,#7b96e1 94%,#7a93df 97%,#abbae3 100%)",
+                    paddingTop: "12px",
+                    paddingBottom: "12px",
+                    borderTop: "1px solid #7b99e1",
+                    borderLeft: "1px solid #7b99e1",
+                    borderRight: "1px solid #7b99e1",
+                  }
+                : {
+                    paddingTop: "12px",
+                    paddingBottom: "12px",
+                  }
+            }
           >
             <div className="title-bar-text flex items-center gap-1">
               <Image
-                src={`/windowsIcons/${program.id}.png`}
+                src={`/icons/${program.id}.png`}
                 alt={`${program.name}`}
                 height={"18"}
                 width={"18"}
@@ -104,7 +111,7 @@ const ProgramWindow = ({ children, program, setProgram, className }: Props) => {
                 }
               />
             </div>
-          </div>
+          </strong>
           {children}
         </div>
       </Draggable>
