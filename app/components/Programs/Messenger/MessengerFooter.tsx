6feb15344587d12dae966c8ffbacf20dca8b965b
rduraj96 <durajrei@gmail.com>
2023-08-31 20:51:17 -0400

    Added MSN Footer

---
+++ app/components/Programs/Messenger/MessengerFooter.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const MessengerFooter = (props: Props) => {
+  return (
+    <div className="font-bold bg-[#FDFDFF] pl-2 flex items-center h-4 text-[11px] border-t-[1px] border-[#505E67]">
+      Contact me here!
+    </div>
+  );
+};
+
+export default MessengerFooter;
