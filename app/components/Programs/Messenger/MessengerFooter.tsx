97076b2f65d50a298570b325ce09a4dfeb4641c5
rduraj96 <durajrei@gmail.com>
2023-09-05 21:25:35 -0400

    Small UI fix

--- app/components/Programs/Messenger/MessengerFooter.tsx
+++ app/components/Programs/Messenger/MessengerFooter.tsx
@@ -4,8 +4,8 @@ type Props = {};
 
 const MessengerFooter = (props: Props) => {
   return (
-    <div className="font-bold bg-[#FDFDFF] pl-2 flex items-center h-4 text-[11px] border-t-[1px] border-[#505E67]">
-      Contact me here!
+    <div className="font-bold bg-[#FDFDFF] pl-2 py-0.5 flex items-center justify-center h-4 text-[11px] text-[#25304c] border-t-[1px] border-[#505E67]">
+      Created by Rei Duraj!
     </div>
   );
 };
