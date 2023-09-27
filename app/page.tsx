5a49b0dc8f10f63bcc750886960112a53c3da283
rduraj96 <durajrei@gmail.com>
2023-09-27 16:54:58 -0400

    Dynamic Loading winamp to fix winow is not defined issued

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-import Image from "next/image";
-import Counter from "./components/Programs/Messenger/Counter";
 import TaskBar from "./components/Footer/TaskBar";
 import Icons from "./components/Icons/Icons";
-import Winamp from "./components/Programs/Winamp/Winamp";
 import Paint from "./components/Programs/Paint/Paint";
 import Messenger from "./components/Programs/Messenger/Messenger";
-import MessengerWindow from "./components/Programs/Messenger/MessengerWindow";
 import Projects from "./components/Programs/MyProjects/Projects";
 import InternetExplorer from "./components/Programs/InternetExplorer/InternetExplorer";
+import dynamic from "next/dynamic";
+
+const Winamp = dynamic(() => import("./components/Programs/Winamp/Winamp"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
@@ -15,10 +16,9 @@ export default function Home() {
       <div className="mt-10 ml-10 w-fit">
         <Icons />
       </div>
+      <Winamp />
       <Projects />
       <InternetExplorer />
-      {/* <MessengerWindow /> */}
-      <Winamp />
       <Paint />
       <Messenger />
       <TaskBar />
