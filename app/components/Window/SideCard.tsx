8be1b87cb7a1533177382e7e2f2a696fc200958a
rduraj96 <durajrei@gmail.com>
2023-11-14 17:04:26 -0500

    Interactive SideCards

--- app/components/Window/SideCard.tsx
+++ app/components/Window/SideCard.tsx
@@ -1,4 +1,6 @@
-import React, { ReactNode } from "react";
+"use client";
+
+import React, { ReactNode, useState } from "react";
 import Image from "next/image";
 
 type Props = {
@@ -7,23 +9,36 @@ type Props = {
 };
 
 const SideCard = (props: Props) => {
+  const [collapsed, setCollapsed] = useState<boolean>(false);
+
   return (
-    <div className="w-full mb-3">
-      <div className="h-6 pl-3 pr-1 rounded-t-[3px] bg-gradient-to-r from-[#f0f0ff] from-[30%] to-[#a8bcff] flex items-center">
-        <p className="font-bold text-[#0c327d] flex-1">{props.title}</p>
+    <div className="w-full mb-3 flex-col">
+      <div
+        className="h-6 pl-3 pr-1 rounded-t-[3px] bg-gradient-to-r from-[#f0f0ff] from-[30%] to-[#a8bcff] flex items-center"
+        onClick={() => setCollapsed((prev) => !prev)}
+      >
+        <p className="font-bold text-[#0c327d] flex-1 hover:text-[#1c68ff] cursor-pointer">
+          {props.title}
+        </p>
         <Image
           src={"/windowsIcons/pullup.png"}
           alt="arrow button"
           height={18}
           width={18}
-          className={`${!props.children && "rotate-180"}`}
+          className={`${
+            (!props.children || collapsed === true) && "rotate-180"
+          }`}
           style={{
             filter: `drop-shadow(1px 1px 3px rgba(0, 0, 0, 0.3))`,
           }}
         />
       </div>
       {props.children && (
-        <div className="px-3 py-2 text-[#0c327d] leading-[14px] text-[10px] bg-gradient-to-r from-[#b4c8fb] via-[#a4b9fb] to-[#b4c8fb]">
+        <div
+          className={`${
+            collapsed === true ? "hidden" : "visible"
+          } px-3 py-2 text-[#0c327d] leading-[14px] text-[10px] bg-gradient-to-r from-[#b4c8fb] via-[#a4b9fb] to-[#b4c8fb]`}
+        >
           {props.children}
         </div>
       )}
