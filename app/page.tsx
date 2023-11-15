61e2b72c2fc969b6b3d02e80be101883eaea907e
rduraj96 <durajrei@gmail.com>
2023-11-15 16:02:56 -0500

    main page use client

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
-import TaskBar from "./components/Footer/TaskBar";
-import Icons from "./components/Icons/Icons";
-import Paint from "./components/Programs/Paint/Paint";
-import Messenger from "./components/Programs/Messenger/Messenger";
-import Projects from "./components/Programs/MyProjects/Projects";
-import InternetExplorer from "./components/Programs/InternetExplorer/InternetExplorer";
+"use client";
+
 import dynamic from "next/dynamic";
-import Monitor from "./components/Monitor";
 import WindowsXp from "./components/WindowsXp";
 
 const Winamp = dynamic(() => import("./components/Programs/Winamp/Winamp"), {
